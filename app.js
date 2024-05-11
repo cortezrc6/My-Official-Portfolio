3d285dd2f631101e023813672d10531874aaa0ed
cortezrc6 <cortezrc6@gmail.com>
2024-05-12 05:52:40 +0800

    Updated icon

--- app.js
+++ app.js
@@ -16,3 +16,16 @@ typeWriter(document.getElementById('typewriter-h2'), "HELLO", 100);
 typeWriter(document.getElementById('typewriter-h3'), "FULLSTACK DEVELOPER", 100);
 typeWriter(document.getElementById('typewriter-p'), "I'm a versatile full-stack developer proficient in both front-end and back-end technologies,", 20);
 typeWriter(document.getElementById('typewriter-p2'), "passionate about crafting seamless digital experiences from concept to deployment.", 20);
+
+
+function fadeInImage(element, delay) {
+    setTimeout(() => {
+        element.classList.add('show');
+    }, delay);
+}
+
+// Call the fadeInImage function for each icon one by one
+fadeInImage(document.getElementById('fb-icon'), 1000); // Delay the appearance of the first icon by 1000 milliseconds (1 second)
+fadeInImage(document.getElementById('instagram-icon'), 1500); // Delay the appearance of the second icon by 2000 milliseconds (2 seconds)
+fadeInImage(document.getElementById('linkedin-icon'), 2000); // Delay the appearance of the third icon by 3000 milliseconds (3 seconds)
+fadeInImage(document.getElementById('upwork-icon'), 2500); // Delay the appearance of the fourth icon by 4000 milliseconds (4 seconds)
